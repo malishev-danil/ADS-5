cb9709d93cbd74ac6bafbd7b3e2f6114d4389892
malishev-danil <malishev-danil5@yandex.ru>
2025-04-19 16:13:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 // Copyright 2021 NNTU-CS
 
 int main() {
+    std::string infixExpression;
+    std::string postfixExpression = infx2pstfx(infixExpression);
+    int result = eval(postfixExpression);
     return 0;
 }
