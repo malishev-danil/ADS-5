42a1e57d79355a41a74bf025237998cfa1f5bf36
malishev-danil <malishev-danil5@yandex.ru>
2025-04-20 11:45:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,119 +1,120 @@
-// Copyright 2025 NNTU-CS
-#include <stack>
+#include <iostream>
 #include <string>
 #include <map>
 #include <sstream>
-#include <cctype>
-#include <stdexcept>
 #include "tstack.h"
-#include "alg.h"
 
-int getOperationPriority(char op) {
-switch (op) {
-case '+':
-case '-':
-return 1;
-case '*':
-case '/':
-return 2;
-default:
-throw std::invalid_argument("Недопустимая операция");
+int priority(char oper) {
+    switch (oper) {
+    case '+':
+    case '-': return 1;
+    case '*':
+    case '/': return 2;
+    default: return 0;
+    }
 }
-}
+std::string infx2pstfx(const std::string& inf) {
+    std::string outS;
+  TStack<char, 100> stack;
+  for (size_t j = 0; j < inf.length(); j++) {
+    char symb = inf[j];
+    if (symb == ' ') {
+        continue;
+    }
 
-std::string infx2pstfx(const std::string& infix) {
-TStack<char, 100> operatorsStack;
-std::stringstream output;
-std::string numberBuffer;
+    if (std::isdigit(symb)) {
+        while (j < inf.length() && std::isdigit(inf[j])) {
+            outS.push_back(inf[j]);
+            j++;
+        }
+        j--;
+        outS.push_back(' ');
+    } else if (symb == '(') {
+        stack.push(symb);
+    } else if (symb == ')') {
+        while (!stack.isEmpty() && stack.top() != '(') {
+            outS.push_back(stack.top());
+            outS.push_back(' ');
+            stack.pop();
+        }
+        if (!stack.isEmpty() && stack.top() == '(') {
+            stack.pop();
+        } else {
+            throw std::runtime_error("Err");
+        }
+    } else if (symb == '+' || symb == '-' || symb == '*' || symb == '/') {
+        while (!stack.isEmpty() && priority(stack.top()) >=
+            priority(symb)) {
+            outS.push_back(stack.top());
+            outS.push_back(' ');
+            stack.pop();
+        }
+        stack.push(symb);
+    } else {
+        throw std::runtime_error(std::string("Err") + symb);
+    }
+  }
+  while (!stack.isEmpty()) {
+    if (stack.top() == '(' || stack.top() == ')') {
+        throw std::runtime_error("Err");
+    }
+    outS.push_back(stack.top());
+    outS.push_back(' ');
+    stack.pop();
+  }
+  if (!outS.empty() && outS.back() == ' ') {
+    outS.pop_back();
+  }
+  return outS;
+}
+int eval(const std::string& pref) {
+    TStack<int, 100> stack;
+    std::istringstream iss(pref);
+    std::string tok;
+    while (iss >> tok) {
+        if (std::isdigit(tok[0])) {
+            int cnt = std::stoi(tok);
+            stack.push(cnt);
+        } else if (tok.length() == 1 &&
+                   (tok[0] == '+' || tok[0] == '-' ||
+                    tok[0] == '*' || tok[0] == '/')) {
+            if (stack.isEmpty()) {
+                throw std::runtime_error("Err");
+            }
+            int operand2 = stack.top();
+            stack.pop();
+            if (stack.isEmpty()) {
+                throw std::runtime_error("Err");
+            }
+            int operand1 = stack.top();
+            stack.pop();
+            int result = 0;
+            switch (tok[0]) {
+                case '+': result = operand1 + operand2; break;
+                case '-': result = operand1 - operand2; break;
+                case '*': result = operand1 * operand2; break;
+                case '/':
+                    if (operand2 == 0) {
+                        throw std::runtime_error("Err");
+                    }
+                    result = operand1 / operand2;
+                    break;
+                default:
+                    throw std::runtime_error("Err");
+            }
+            stack.push(result);
+        } else {
+            throw std::runtime_error("Err");
+        }
+    }
+    if (stack.isEmpty()) {
+        throw std::runtime_error("Empty");
+    }
+    int res = stack.top();
+    stack.pop();
 
-for (char currentChar : infix) {
-if (isdigit(currentChar)) {
-numberBuffer += currentChar;
-} else {
-if (!numberBuffer.empty()) {
-output << numberBuffer << " ";
-numberBuffer.clear();
-}
-if (currentChar == '(') {
-operatorsStack.push(currentChar);
-} else if (currentChar == ')') {
-while (!operatorsStack.isEmpty() && operatorsStack.peek() != '(') {
-output << operatorsStack.pop() << " ";
-}
-if (!operatorsStack.isEmpty()) {
-operatorsStack.pop();
-}
-} else if (currentChar == '+' || currentChar == '-' ||
-currentChar == '*' || currentChar == '/') {
-while (!operatorsStack.isEmpty() &&
-getOperationPriority(operatorsStack.peek()) >=
-getOperationPriority(currentChar)) {
-output << operatorsStack.pop() << " ";
-}
-operatorsStack.push(currentChar);
-} else if (!isspace(currentChar)) {
-throw std::invalid_argument("Недопустимый символ в выражении");
-}
-}
-}
-
-if (!numberBuffer.empty()) {
-output << numberBuffer << " ";
-}
-
-while (!operatorsStack.isEmpty()) {
-output << operatorsStack.pop() << " ";
-}
-
-std::string postfixResult = output.str();
-if (!postfixResult.empty() && postfixResult.back() == ' ') {
-postfixResult.pop_back();
-}
-return postfixResult;
-}
-
-int eval(const std::string& postfix) {
-TStack<int, 100> evaluationStack;
-std::stringstream ss(postfix);
-std::string token;
-
-while (ss >> token) {
-if (isdigit(token[0])) {
-evaluationStack.push(std::stoi(token));
-} else {
-if (evaluationStack.isEmpty()) {
-throw std::runtime_error("Ошибка: недостаточно операндов");
-}
-int operand2 = evaluationStack.pop();
-if (evaluationStack.isEmpty()) {
-throw std::runtime_error("Ошибка: недостаточно операндов");
-}
-int operand1 = evaluationStack.pop();
-
-switch (token[0]) {
-case '+':
-evaluationStack.push(operand1 + operand2);
-break;
-case '-':
-evaluationStack.push(operand1 - operand2);
-break;
-case '*':
-evaluationStack.push(operand1 * operand2);
-break;
-case '/':
-if (operand2 == 0) {
-throw std::invalid_argument("Ошибка: деление на ноль");
-}
-evaluationStack.push(operand1 / operand2);
-break;
-default:
-throw std::invalid_argument("Недопустимая операция");
-}
-}
-}
-
-if (evaluationStack.size() != 1) {
-throw std::runtime_error("Ошибка: некорректное выражение");
-}
-return evaluationStack.pop();
+    if (!stack.isEmpty()) {
+        throw std::runtime_error("erroneously");
+    }
+    return res;
 }
